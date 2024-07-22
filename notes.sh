afd81e675007b0c73b14af912e7bb1467ab710d4
Rasool Saghaleyni <rasool.saghaleyni@gmail.com>
2024-07-22 14:25:14 +0200

    Add supervised lab

--- notes.sh
+++ notes.sh
@@ -1,4 +1,5 @@
-docker build --platform=linux/amd64 -t docker.io/rasoolsnbis/omicsint_h24:unsupervisedOMICsIntegration-1.0.0 -f "session_ml/UnsupervisedOMICsIntegration/Dockerfile" "session_ml/UnsupervisedOMICsIntegration"
+docker build --platform=linux/amd64 -t docker.io/rasoolsnbis/omicsint_h24:supervisedOMICsIntegration-1.0.0 -f "session_ml/SupervisedOMICsIntegration/Dockerfile" "session_ml/SupervisedOMICsIntegration"
 
-docker run -d -p 8787:8787 --platform=linux/amd64 --name my_rstudio_container docker.io/rasoolsnbis/omicsint_h24:unsupervisedOMICsIntegration-1.0.0
+docker run -d -p 8787:8787 --platform=linux/amd64 --name supervisedOMICsIntegration docker.io/rasoolsnbis/omicsint_h24:supervisedOMICsIntegration-1.0.0
 
+docker run -d -p 8787:8787 --platform=linux/amd64 --name unsupervisedOMICsIntegration docker.io/rasoolsnbis/omicsint_h24:unsupervisedOMICsIntegration-1.0.0
