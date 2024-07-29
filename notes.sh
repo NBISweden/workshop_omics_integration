9fbd897c804e1443b522adf19551ebb0e604e7bc
Rasool Saghaleyni <rasool.saghaleyni@gmail.com>
2024-07-29 13:40:18 +0200

    Add GEM GSA lab

--- notes.sh
+++ notes.sh
@@ -1,4 +1,4 @@
-docker build --platform=linux/amd64 -t docker.io/rasoolsnbis/omicsint_h24:session_mofa-1.0.0 -f "session_mofa/Dockerfile" "session_mofa"
+docker build --platform=linux/amd64 -t docker.io/rasoolsnbis/omicsint_h24:session_gsa-1.0.0 -f "session_gsa/Dockerfile" "session_gsa"
 
 docker run -d -p 8787:8787 --platform=linux/amd64 --name session_mofa docker.io/rasoolsnbis/omicsint_h24:session_mofa-1.0.0
 
