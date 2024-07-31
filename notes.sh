41bd1fe22db041398119a6307e4f8f7c9bf01247
Rasool Saghaleyni <rasool.saghaleyni@gmail.com>
2024-07-31 10:40:45 +0200

    Add topology lab

--- notes.sh
+++ notes.sh
@@ -1,7 +1,10 @@
-docker build --platform=linux/amd64 -t docker.io/rasoolsnbis/omicsint_h24:session_ml_umap-1.0.0 -f "session_ml/UMAP_DataIntegration/Dockerfile" "session_ml/UMAP_DataIntegration/"
+docker build --platform=linux/amd64 -t docker.io/rasoolsnbis/omicsint_h24:session_topology-1.0.0 -f "session_topology/Dockerfile" "session_topology/"
 
-docker run -d -p 8787:8787 --platform=linux/amd64 --name session_mofa docker.io/rasoolsnbis/omicsint_h24:session_mofa-1.0.0
 
-docker run -d -p 8787:8787 --platform=linux/amd64 --name supervisedOMICsIntegration docker.io/rasoolsnbis/omicsint_h24:supervisedOMICsIntegration-1.0.0
 
-docker run -d -p 8787:8787 --platform=linux/amd64 --name unsupervisedOMICsIntegration docker.io/rasoolsnbis/omicsint_h24:unsupervisedOMICsIntegration-1.0.0
+Labs:
+- In the Supervised omics integration lab I generated the docker with its relevant conda env activated within the docker and hosted the docker on scilifelab serve project, but in line 188 of notebook it returns an error, which I couldn’t initially figure it out what is the source of it.
+
+- Session meta lab is problematic for making the required env.
+
+- Session topology lab is running well, but code chunk 21 is problematic, which I couldn’t figure out what is the source of it. 
